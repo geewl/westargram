f056acc10274b53ba64e6082537b2a7f36abc296
geewl <gee902722@gmail.com>
2022-02-13 20:54:36 +0900

    update

---
+++ ex/my_settings.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test',
+        'USER': 'root',
+        'PASSWORD': '0318',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
+
+SECRET_KEY = 'django-insecure--6dj%wz628$1!#z%&0+twkood$*1%k&o7!e(p1dr(6@q0b#7)@'
---
+++ my_settings.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'hw1',
+        'USER': 'root',
+        'PASSWORD': '0318',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
+
+SECRET_KEY = 'django-insecure-&6b*!g6xu6e3%du0^87m^(2oyc%&_=02ks=^$g_8^9^akx(rxn'
